aa37016c70d74894c8f018b7cb988b6d56412c80
chavdaraj911 <maliraj052@gmail.com>
2021-05-20 14:48:42 +0530

    Simple mvc practical
    mvc practical for insert data via model without database.

---
+++ login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcformvc.Models
+{
+    public class login
+    {
+        public int id { get; set; }
+        public string name {get;set;}
+        public string login_id { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ mvc_in_mvcController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcformvc.Models;
+
+namespace mvcformvc.Controllers
+{
+    public class mvc_in_mvcController : Controller
+    {
+        // GET: mvc_in_mvc
+        public ActionResult Index()
+        {
+            var data = GetLogin();
+            return View(data);
+        }
+        private login GetLogin()
+        {
+            return new login()
+            {
+                id = 10,
+                name = "Dhoni",
+                login_id = "Dhoni12@gmail.com",
+                password = "Dhoni_mahi"
+
+            };
+        }
+    }
+}
