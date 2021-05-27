837a7e62227e81f65ada184d223b53db779836c7
chavdaraj911 <maliraj052@gmail.com>
2021-05-27 16:38:49 +0530

    heper_models

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Html_Helper1.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Enter name")]
+        public string Name { get; set; }
+        public string email { get; set; }
+        public bool isonline { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime dateofbirth { get; set; }
+    }
+}
---
+++ strongly.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Html_Helper1.Models
+{
+    public class strongly
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public bool isemployee { get; set; }
+
+    }
+}
