93236df170a1063260eb5e6e26b6072cdcf38e31
chavdaraj911 <maliraj052@gmail.com>
2021-06-01 15:50:27 +0530

    traditional_routing_controllerfile

--- HomeController.cs
+++ HomeController.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using traditional_routing.Models;
 
-namespace razor_satements.Controllers
+namespace traditional_routing.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult getallstudents()
         {
             return View();
+        }
+
+        public ActionResult getstudent(int id)
+        {
+            return View();
+        }
+
+        public ActionResult getallstudentaddress(int id)
+        {
+            return View();
+        }
+        private List<Student> Students()
+        {
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    id = 1,
+                    name = "Raj",
+                    clas = "Frist",
+
+                    Address = new Address()
+                    {
+                        stAddress = "this is i address",
+                    };
+            
+                }
+
+            }
         }
     }
 }
